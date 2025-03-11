006ec3594915ea2019d65da6fdb6c0c02a3c4f31
Rattini <rattinicursods@gmail.com>
2025-03-11 14:52:14 -0300

    Adição de filmes

---
+++ 04-devflix/src/components/movieCard/MovieCard.jsx
@@ -0,0 +1,18 @@
+import styles from './MovieCard.module.css'
+
+const MovieCard = (props) => {
+  return (
+    <div className={styles.movie}>
+    <div>
+      <p>{props.Year}</p>
+    </div>
+
+    <img src={props.Poster} alt="" />
+    <h4>{props.Type}</h4>
+    <h3>{props.Title}</h3>
+    <p>{props.Description}</p>
+  </div>
+  )
+}
+
+export default MovieCard
