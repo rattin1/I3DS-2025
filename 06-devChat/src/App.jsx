477820d2fa54a340adadf8fc0ec82ab98ab4212f
Rattini <rattinicursods@gmail.com>
2025-04-09 17:02:41 -0300

    Momento que desisti de fazer o backend e comecei o frontend

--- 06-devChat/src/App.jsx
+++ 06-devChat/src/App.jsx
@@ -14,14 +14,7 @@ const [socket, setSocket] = useState(null);
   return (
     <div id="App" className='m-0 p-0 vh-100 d-flex flex-column justify-content-center align-items-center bg-dark text-light'>
       {chatVisibility ?<Chat socket={socket} /> : <Join setSocket={setSocket} visibility={setChatVisibility} />}
-
-      <div>
-        
-      </div>
     </div>
-    
-
-    
   )
 }
 
